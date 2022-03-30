0defa585fe8d0d6487e9ace1695aa9fa84e473be
Bryan <bryanandagoya@gmail.com>
2022-03-30 09:01:25 -0500

    register services

--- service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
+++ service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
@@ -2,12 +2,14 @@ package academy.digitallab.store.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceProductApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductApplication.class, args);
+    }
 
 }
